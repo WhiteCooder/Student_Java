dfec56a4dc6e4a5eb135891f4053426e5fb3991c
WHITE CODER <whitecooder@gmail.com>
2021-12-18 17:48:03 +0500

    SimpleStudentManagementSystem

---
+++ Student/Project File/StudentManagementSystem/src/jTable1.java
@@ -0,0 +1,20 @@
+
+import javax.swing.table.TableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ali
+ */
+class jTable1 {
+
+    static void setModel(TableModel resultSetToTableModel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
